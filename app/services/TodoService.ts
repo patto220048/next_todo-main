e98f2b3c4cefd6f218ea8c74a42a9da4355b1533
patto220048 <220048xn@yse-c.net>
2024-02-06 12:09:28 +0900

    first commit

---
+++ app/services/TodoService.ts
@@ -0,0 +1,42 @@
+
+import { POST } from "../api/todo/add/route";
+import { Todo } from "../models/Todo";
+export interface Todos {
+    value: string;
+    tags: string[]
+}
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "";
+
+export const getTodos = async () => {
+    //TODO: API URL設定
+    const url = API_URL + "todo/get";
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            return await response.json();
+        }
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const postTodos = async (todos: Todo[]) => {
+    if (!todos) return;
+    //TODO: API URL設定
+    const url = API_URL + "todo/add";
+ 
+    const data = JSON.stringify(todos);
+    console.log(data);
+    //TODO: APIで保存し、データを返す
+    try {
+        const response = await fetch(url,{
+            method : "POST",
+            body : data
+        }); 
+        return response.json()
+     } catch (error) {
+         console.error(error)
+         
+     }
+
+}
